b82eddcabc1a9ae7253fdcf3b52c8cc37827df9b
Leo-Mugambi <leo.bikuri@strathmore.edu>
2022-02-15 13:57:55 +0300

    The start

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/1b51ddf955.js" crossorigin="anonymous"></script>
+    <link rel="icon" href="/images/favicon.png">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+     <style >
+         @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&family=Ropa+Sans&display=swap');
+         body{
+             font-family: 'Ropa Sans', sans-serif;
+         }
+     </style>
+    <title>Policy and Market Options</title>
+</head>
+<body>
+@include('partials.nav')
+@yield('content')
+</body>
+</html>
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row pt-5 px-5">
+        <div class="col-9 d-flex">
+             <img src="{{asset('/images/cropped-logo.png')}}" style="width: 61px; height: 40px;" class="">
+             <h1>Policy & Market Options</h1>
+        </div>
+        <div class="col-3">
+
+        </div>
+    </div>
+</div>
